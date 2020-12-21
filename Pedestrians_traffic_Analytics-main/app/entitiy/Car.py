7f6b8829f7a2fd3aa9a17d25c43c93b254429b23
youssef-naj <60883862+youssef-naj@users.noreply.github.com>
2020-12-21 13:45:31 +0100

    Lmohimthis file has pedestrians + vehicules spawn the issue is the generated FCD is empty no matter what i think it's due to generating data from a python script which isn't the case for normal ussage of sumo (even after or while running the simulation it's always empty)

---
+++ Pedestrians_traffic_Analytics-main/app/entitiy/Car.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3257a40692b1b76598e5f1a79996906ed6db0d13223b8866b4ebad4f7390e642
+size 9243
---
+++ Pedestrians_traffic_Analytics-main/app/entitiy/CarRegistry.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7ed7ee097b10bfea2aee2ac1de0d1ce191d30fe2e73f2f5865b9059b1b9bf3a
+size 1964
